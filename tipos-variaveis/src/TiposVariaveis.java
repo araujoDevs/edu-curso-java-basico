0080c0863447933f45dde6b5bdc269fad6196a78
araujo <araujo448@outlook.com>
2024-06-15 19:57:57 -0300

    add tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        short quantidadeDeProdutos = 10;
+        int quantidadeDeFuncionarios = 50;
+        long quantidadeDeClientes = 100000L;
+        float pi = 3.14f;
+        char sexo = 'M';
+        boolean isAdmin = true;
+        String nome = "João da Silva";
+        System.out.println(quantidadeDeClientes);
     }
 }
