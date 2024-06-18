4de9f65161490642d5fe4d997ed08610f124e86e
araujo <araujo448@outlook.com>
2024-06-17 22:57:01 -0300

    foi alterado para Tipos_variaveis

--- tipos-variaveis/src/Tipos_variaveis.java
+++ tipos-variaveis/src/Tipos_variaveis.java
@@ -1,4 +1,4 @@
-public class TiposVariaveis {
+public class Tipos_variaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
         short quantidadeDeProdutos = 10;
---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,7 @@
+public class operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM " + " JAVA";
+        System.out.println(nomeCompleto);
+        
+    }
+}
